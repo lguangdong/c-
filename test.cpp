4e9880cbb0481d0a823b15da31711ae0080c5a17
mMac <liguangdong@192.168.0.101>
2019-08-05 22:02:45 +0800

    .

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cctype>
+using namespace std;
+void sayHello(ostream any_out_stream);
+int pluss(int a,int b){
+    cout << "普通函数掉调用" <<endl;
+    return a + b;
+};
+template <class T>
+int pluss(T a,T b){
+    cout << "泛型函数调用" <<endl;
+    return a + b;
+};
+int main(){
+    ofstream fout;
+    char input[] = "hello word!";
+    string input2 = "hello word";
+    fout.open("test.txt");
+    if(fout.fail()){
+        cout<<"open file failed!";
+        return 0;
+    }
+    char next;
+    fout.put(*input);
+    //sayHello(fout);
+    fout.close();
+    //ostream.put(input);
+    char a = toupper('a');
+    cout << toupper('a') <<endl;
+    cout << a <<endl;
+    cout << static_cast<char>(toupper('b')) <<endl;
+
+
+    //=====array=======
+    int score[5];
+    int c = 10,d = 20;
+    pluss(c,d);
+    pluss<int>(c,d);
+
+    return 0;
+}
+
+void sayHello(ostream& any_out_stream){
+    any_out_stream << "hello";
+}
